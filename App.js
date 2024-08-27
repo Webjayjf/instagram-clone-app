6f0b565ae109bcbc995ea9b189dadb0d1a4b1201
Webjayjf <moletemolebatsiweb@gmail.com>
2024-08-28 00:04:37 +0200

    Update App.js

--- App.js
+++ App.js
@@ -11,7 +11,6 @@ import {
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import LoginForm from "./src/components/login/LoginForm";
-import Footer from "./src/components/login/Footer";
 import FastImage from "react-native-fast-image";
 
 const LoginScreen = ({ navigation }) => {
@@ -46,7 +45,7 @@ const LoginScreen = ({ navigation }) => {
             </View>
           </View>
         </KeyboardAvoidingView>
-        <Footer navigation={navigation} />
+        
       </SafeAreaView>
     </TouchableWithoutFeedback>
   );
