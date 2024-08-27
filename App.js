a428b28777b999acb7ebd704b7f5acd7ba210f8c
Webjayjf <moletemolebatsiweb@gmail.com>
2024-08-27 23:14:23 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
-import AuthNavigation from './src/navigation/AuthNavigation';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { View, Text } from 'react-native';
 
 export default function App() {
@@ -10,11 +8,5 @@ export default function App() {
       <Text>This is text inside a View</Text>
     </View>
   );
-}
+}; 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
